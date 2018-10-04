cbe81d0672429285c4b32e473f3e13b47fbf547e
Myles Shannon <mylios101@gmail.com>
2018-10-04 16:59:13 -0400

    project1 completed

--- project1/script.js
+++ project1/script.js
@@ -9,12 +9,17 @@ function proxyCall(func) {
 var obj = {};
  
 
- 
+
 // Inplement a bind function similar to ( $.proxy and _.bind),
 function bind(func, obj) {
-    throw Error("Not Implemented!");
-}
+    let prevArgs = [].slice.call(arguments, 2);
+    return function() {
+        let args = [].slice.call(arguments);
+        return func.apply(obj, [].concat(prevArgs, args));
+    }
+};
  
 // so that:                            
 proxyCall(bind(printMyThisAndArgs, obj)); // obj  []                                              
 bind(printMyThisAndArgs, obj)(1,2,3); // obj [1,2,3]
+bind(printMyThisAndArgs, obj, 0)(1,2,3); // obj [0,1,2,3]
