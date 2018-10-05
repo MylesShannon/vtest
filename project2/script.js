70ccd5ba2f5d028db124e96cb1c7ec87b5fcb77c
Myles Shannon <mylios101@gmail.com>
2018-10-04 21:31:51 -0400

    project2 completed

--- project2/script.js
+++ project2/script.js
@@ -14,7 +14,31 @@ EventDispatcher = Class.extend({
  
     // constructor
     init : function() {
+        this.events = {};
+    },
 
+    /**
+     * 
+     * Return all events
+     *
+     **/
+    getEvents : function() {
+        return this.events;
+    },
+
+
+    /**
+     * 
+     * Find and return event by name  
+     *
+     **/
+    findEvent : function(eventName) {
+        // Type check
+        if(typeof eventName === "string") {
+            return this.events[eventName.toLowerCase()] || this.events[eventName.toUpperCase()] || false;
+        } else {
+            throw Error("findEvent: requires event name")
+        }
     },
    
  
@@ -30,7 +54,21 @@ EventDispatcher = Class.extend({
      *
      **/
     addListener : function(eventName, callback, context) {
-    
+        // Type check, context is optional 
+        if(typeof eventName === "string" && typeof callback === "function" && context === undefined ? true : typeof context === "object") {
+            // Persistent events are stored with a lower-case event name
+            let persistentEventName = eventName.toLowerCase();
+            // Check if the event already exists, if not, initialize it with an array
+            this.events[persistentEventName] = this.events[persistentEventName] || [];
+            // Bind the context to the listener if a context is provided, else just push the listener to the event
+            if(context !== undefined) {
+                this.events[persistentEventName].push(callback.bind(context));
+            } else {
+                this.events[persistentEventName].push(callback);
+            }
+        } else {
+            throw Error("addListener: Missing event name and/or callback")
+        }
     },
  
  
@@ -43,7 +81,21 @@ EventDispatcher = Class.extend({
      *
      **/
     addOneTimeListener : function(eventName, callback, context) {
-
+        // Type check
+        if(typeof eventName === "string" && typeof callback === "function") {
+            // Temporary events are stored with an upper-case event name
+            let temporaryEventName = eventName.toUpperCase();
+            // Check if the event already exists, if not, initialize it with an array
+            this.events[temporaryEventName] = this.events[temporaryEventName] || [];
+            // Bind the context to the listener if a context is provided, else just push the listener to the event
+            if(context !== undefined) {
+                this.events[temporaryEventName].push(callback.bind(context));
+            } else {
+                this.events[temporaryEventName].push(callback);
+            }
+        } else {
+            throw Error("addOneTimeListener: Missing event name and/or callback")
+        }
     },
  
  
@@ -58,7 +110,39 @@ EventDispatcher = Class.extend({
      *
      **/
     removeListener : function(eventName, callback) {
-    
+        // Type check
+        if(typeof eventName === "string" && callback === undefined ? true : typeof callback === "function") {
+            let persistentEventName = eventName.toLowerCase();
+            let temporaryEventName = eventName.toUpperCase();
+            let remove = function(eventName) {
+                // If a callback argument is provided, remove only that listener from the event, else remove the entire event
+                if(callback !== undefined) {
+                    for(let i = 0; i < this.events[eventName].length; i++) {
+                        if(this.events[eventName][i] === callback) {
+                            this.events[eventName].splice(i, 1);
+                            break;
+                        }
+                    }
+                } else {
+                    delete this.events[eventName];
+                }
+            };
+
+            // If either a temporary or persistent event exists, remove them both
+            if(this.events[temporaryEventName] || this.events[persistentEventName]) {
+                if(this.events[persistentEventName]) {
+                    remove.call(this, persistentEventName);
+                }
+                if(this.events[temporaryEventName]) {
+                    remove.call(this, temporaryEventName);
+                }
+                return;
+            }
+
+            throw Error("removeListener: No listeners for event");
+        } else {
+            throw Error("removeListener: Missing event name");
+        }
     },
  
  
@@ -73,30 +157,35 @@ EventDispatcher = Class.extend({
      *
      **/
     triggerEvent : function(eventName, otherParams) {
-
+        // If given more than one otherParam argument, make an array of them
+        if(arguments.length > 2) {
+            otherParams = [].slice.call(arguments, 1);
+        }
+        // Type check
+        if(typeof eventName === "string") {
+            let temporaryEventName = eventName.toUpperCase();
+            let persistentEventName = eventName.toLowerCase();
+            // If either a temporary or persistent event exists
+            if(this.events[persistentEventName] || this.events[temporaryEventName]) {
+                // If a persistent event exists, trigger its listeners
+                if(this.events[persistentEventName]) {
+                    for(let i = 0; i < this.events[persistentEventName].length; i++) {
+                        this.events[persistentEventName][i](eventName, otherParams);
+                    }
+                }
+                // If a temporary event exists, trigger its listeners, then remove that event
+                if(this.events[temporaryEventName]) {
+                    for(let i = 0; i < this.events[temporaryEventName].length; i++) {
+                        this.events[temporaryEventName][i](eventName, otherParams);
+                    }
+                    delete this.events[temporaryEventName];
+                }
+                return;
+            }
+            
+            throw Error("triggerEvent: No listeners for event");
+        } else {
+            throw Error("triggerEvent: Missing event name");
+        }
     }
- 
- 
 });
-
-
-
-
-function exampleUsage() {
-
-    var eventBus = new EventDispatcher();
-    var greetToConsole = function(eventName, msg) { console.log("hello from " + msg); };
-    var greetToAlert = function(eventName, msg) { alert("hello from " + msg); };
- 
-
-    eventBus.addListener("GeetEvent", greetToConsole);
-    eventBus.addListener("GeetEvent", greetToAlert);
-
-    eventBus.triggerEvent("GeetEvent", "volicon"); // will alert and print to console "hello from volicon"    
-    eventBus.removeListener("GeetEvent", greetToAlert);
-    eventBus.triggerEvent("GeetEvent", "your console"); // will only print to console...
-
-}
-
-
-exampleUsage();
--- project2/tests.js
+++ project2/tests.js
@@ -1,7 +1,205 @@
-module("EventDispacher")
+QUnit.module("EventDispacher")
 
-test("here be test", function() {
+QUnit.test("Add a listener and trigger it", function(assert) {
+    let eventBus = new EventDispatcher();
+    eventBus.addListener("TestEvent", function(eventName, msg) {
+        assert.strictEqual(undefined, msg);
+        assert.strictEqual("TestEvent", eventName);
+    });
+    eventBus.triggerEvent("TestEvent");
+    assert.expect(2);
+});
 
-    strictEqual(false, true);
+QUnit.test("Add a listener and trigger it with an argument", function(assert) {
+    let eventBus = new EventDispatcher();
+    eventBus.addListener("TestEvent", function(eventName, msg) {
+        assert.strictEqual("TestEvent", eventName);
+        assert.strictEqual("This is only a test!", msg);
+    });
+    eventBus.triggerEvent("TestEvent", "This is only a test!");
+    assert.expect(2);
+});
 
+QUnit.test("Add a listener and trigger it with multiple arguments", function(assert) {
+    let eventBus = new EventDispatcher();
+    eventBus.addListener("TestEvent", function(eventName, msg) {
+        assert.strictEqual("This is only a test! This is also only a test!", msg[0]+" "+msg[1]);
+        assert.strictEqual("TestEvent", eventName);
+    });
+    eventBus.triggerEvent("TestEvent", "This is only a test!", "This is also only a test!");
+    assert.expect(2);
+});
+
+QUnit.test("Add a listener and trigger it with an array", function(assert) {
+    let eventBus = new EventDispatcher();
+    eventBus.addListener("TestEvent", function(eventName, msg) {
+        assert.strictEqual("This is only a test! This is also only a test!", msg[0]+" "+msg[1]);
+        assert.strictEqual("TestEvent", eventName);
+    });
+    eventBus.triggerEvent("TestEvent", ["This is only a test!", "This is also only a test!"]);
+    assert.expect(2);
+});
+
+QUnit.test("Add two listener, trigger both, remove one, trigger the last one again", function(assert) {
+    var eventBus = new EventDispatcher();
+    var greet = function(eventName, msg) {
+        assert.step("hello from " + msg);
+    };
+    var earthToo = function(eventName, msg) {
+        assert.step("hello from " + msg + " and Earth");
+    };
+
+    eventBus.addListener("GeetEvent", greet);
+    eventBus.addListener("GeetEvent", earthToo);
+    eventBus.triggerEvent("GeetEvent", "Volicon");
+    eventBus.removeListener("GeetEvent", earthToo);
+    eventBus.triggerEvent("GeetEvent", "Volicon");
+
+    assert.verifySteps(["hello from Volicon", "hello from Volicon and Earth", "hello from Volicon"]);
+});
+
+QUnit.test("Add a listener, trigger it, remove it, add two listeners, trigger them both", function(assert) {
+    let eventBus = new EventDispatcher();
+    let test = function(eventName, msg) {
+        assert.step(msg);
+    };
+    eventBus.addListener("TestEvent", test);
+    eventBus.triggerEvent("TestEvent", "1");
+    eventBus.removeListener("TestEvent");
+    eventBus.addListener("TestEvent", test);
+    eventBus.addListener("TestEvent", test);
+    eventBus.triggerEvent("TestEvent", "2");
+    assert.verifySteps(["1", "2", "2"]);
+});
+
+QUnit.test("Add a one-time listener, trigger it, and check if it still exists", function(assert) {
+    let eventBus = new EventDispatcher();
+    eventBus.addOneTimeListener("TestEvent", function(eventName, msg) {
+        assert.strictEqual("This is only a test!", msg);
+    });
+    eventBus.triggerEvent("TestEvent", "This is only a test!");
+    assert.notStrictEqual(eventBus.findEvent("TestEvent"));
+    assert.expect(2);
+});
+
+QUnit.test("Add three one-time listeners, trigger them, and check if they still exist", function(assert) {
+    let eventBus = new EventDispatcher();
+    let test = function(eventName, msg) {
+        assert.strictEqual("This is only a test!", msg);
+    };
+    eventBus.addOneTimeListener("TestEvent", test);
+    eventBus.addOneTimeListener("TestEvent", test);
+    eventBus.addOneTimeListener("TestEvent", test);
+    eventBus.triggerEvent("TestEvent", "This is only a test!");
+    assert.notStrictEqual(eventBus.findEvent("TestEvent"));
+    assert.expect(4);
+});
+
+QUnit.test("Add two listeners and two one-time listeners, then remove the first two", function(assert) {
+    let eventBus = new EventDispatcher();
+    eventBus.addListener("TestEvent", function() {});
+    eventBus.addOneTimeListener("TestEvent", function() {});
+    eventBus.addListener("AnotherTestEvent", function() {});
+    eventBus.addOneTimeListener("AnotherTestEvent", function() {});
+    eventBus.removeListener("TestEvent");
+    assert.strictEqual(_.size(eventBus.getEvents()), 2);
+    assert.expect(1);
+});
+
+QUnit.test("Check that event names are not case-sensitive", function(assert) {
+    let eventBus = new EventDispatcher();
+    let test = function(eventName, msg) {
+        assert.strictEqual("This is only a test!", msg);
+    };
+    eventBus.addListener("TestEvent", test);
+    eventBus.addListener("tesTevenT", test);
+    eventBus.triggerEvent("TESTEVENT", "This is only a test!");
+    eventBus.triggerEvent("testevent", "This is only a test!");
+    assert.expect(4);
+});
+
+QUnit.test("Add a listener with context and trigger it, check that context persists", function(assert) {
+    let eventBus = new EventDispatcher();
+    eventBus.addListener(
+        "TestEvent",
+        function(eventName, msg) {
+            assert.strictEqual("TestEvent", eventName);
+            assert.strictEqual("Welcome to Boston!", msg+this.city+"!");
+        },
+        {
+            city: "Boston"
+        }
+    );
+    eventBus.triggerEvent("TestEvent", "Welcome to ");
+    assert.expect(2);
+});
+
+QUnit.test("Add a one-time listener with context, trigger it, and check if it still exists", function(assert) {
+    let eventBus = new EventDispatcher();
+    eventBus.addOneTimeListener(
+        "TestEvent", 
+        function(eventName, msg) {
+            assert.strictEqual("TestEvent", eventName);
+            assert.strictEqual("Hello, my name is Rick Astley", msg+this.firstName+" "+this.lastName);
+        },
+        {
+            firstName: "Rick",
+            lastName: "Astley"
+        }
+    );
+    eventBus.triggerEvent("TestEvent", "Hello, my name is ");
+    assert.notStrictEqual(eventBus.findEvent("TestEvent"));
+    assert.expect(3);
+});
+
+QUnit.test("Add a listener and a one-time listener to an event, trigger them both twice, check that one remains", function(assert) {
+    let eventBus = new EventDispatcher();
+    let test = function(eventName, msg) {
+        assert.step(msg);
+    };
+    eventBus.addListener("TestEvent", test);
+    eventBus.addOneTimeListener("TestEvent", test);
+    eventBus.triggerEvent("TestEvent", "1");
+    eventBus.triggerEvent("TestEvent", "2");
+    
+    assert.strictEqual(_.size(eventBus.getEvents()), 1);
+    assert.verifySteps(["1", "1", "2"]);
+    assert.expect(5);
+});
+
+QUnit.test("Malformed event name", function(assert) {
+    let eventBus = new EventDispatcher();
+    let test = function() {
+        eventBus.addListener(
+            1234,
+            function() {}
+        );
+    };
+    assert.throws(test);
+    assert.expect(1);
+});
+
+QUnit.test("Malformed callback", function(assert) {
+    let eventBus = new EventDispatcher();
+    let test = function() {
+        eventBus.addListener(
+            "TestEvent", 
+            1234
+        );
+    };
+    assert.throws(test);
+    assert.expect(1);
+});
+
+QUnit.test("Malformed context", function(assert) {
+    let eventBus = new EventDispatcher();
+    let test = function() {
+        eventBus.addListener(
+            "TestEvent", 
+            function() {},
+            1234
+        );
+    };
+    assert.throws(test);
+    assert.expect(1);
 });
